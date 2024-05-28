16d6e615121b13422b63cfebeb0a3a656a65a651
Antonio <robledoy@uci.edu>
2024-05-27 22:21:42 -0700

    begin wokring on login form and dashboard

---
+++ app/lib/actions.ts
@@ -0,0 +1,36 @@
+'use server'
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import Form from '../ui/patients/create-form';
+import { signIn } from '@/auth';
+import { AuthError } from 'next/auth';
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid Credentials';
+                    default:
+                        return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
+}
+
+const FormSchema = z.object({
+    id: z.string(),
+    patientId: z.string({
+        invalid_type_error: 'Please select a patient,',
+    }),
+
+})
--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,23 @@ import styles from '@/app/ui/home.module.css';
 export default function Home() {
 
   return (
-    <div className={styles.overlay}>
-      <button className="border-2 border-slate-300 rounded-full px-6 py-2 hover:bg-sky-600 hover:text-white text-slate-700 text-xl">Login</button>
-      <button className="border-2 border-slate-300 rounded-full px-4 py-2 hover:bg-sky-600 hover:text-white text-slate-700 text-xl">Sign Up</button>
-        <div className={styles.content}>
-          <h1> Welcome to Clinic Flow: A Clinic Management System for Healthcare Workers.</h1>
-          <img src="https://www.freeiconspng.com/uploads/medical-caduceus-png-16.png" width="30%" alt="Caduceus" />
+    <main className='flex min-h-screen flex-col'>
+      <div className={styles.overlay}>
+        <div className='flex justify-end'>
+        <button className="border-2 border-slate-300 rounded-full px-6 py-2 hover:bg-sky-600 hover:text-white text-slate-700 text-xl">Login</button>
+        <button className="border-2 border-slate-300 rounded-full px-4 py-2 hover:bg-sky-600 hover:text-white text-slate-700 text-xl">Sign Up</button>
         </div>
-    </div>
-
+          <div className={styles.content}>
+            <h1> Welcome to Clinic Flow: A Clinic Management System for Healthcare Workers.</h1>
+            <Image 
+              src="/Caduceus.png"
+              width={200}
+              height={300}
+              className='caduceus'
+              alt='Caduceus symbol'
+              />
+          </div>
+      </div>
+    </main>
     )
 }
-
